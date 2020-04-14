6e1fe97a0ca8b397bc2d6dbedec4c10b8ad29392
brandonktran <brandontran@berkeley.edu>
2020-04-14 02:02:52 -0700

    formatNumber

--- src/index.js
+++ src/index.js
@@ -1,48 +1,47 @@
 function search_country() {
-  let input = document.getElementById('searchbar').value
-  input = input.toLowerCase();
-  let x = document.getElementsByClassName('data');
+	let input = document.getElementById('searchbar').value;
+	input = input.toLowerCase();
+	let x = document.getElementsByClassName('data');
 
-  for (i = 0; i < x.length; i++) {
-    if (!x[i].childNodes[1].textContent.toLowerCase().includes(input)) {
-      x[i].style.display = "none";
-    }
-    else {
-      x[i].style.display = "inline-block"
-    }
-  }
+	for (i = 0; i < x.length; i++) {
+		if (!x[i].childNodes[1].textContent.toLowerCase().includes(input)) {
+			x[i].style.display = 'none';
+		} else {
+			x[i].style.display = 'inline-block';
+		}
+	}
 }
 
 function formatNumber(num) {
-  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+	if (num) {
+		return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+	}
 }
 
 document.getElementById('getData').addEventListener('click', getData);
 
 function getData() {
-  // fetch('https://corona.lmao.ninja/all')
-  fetch('https://coronavirus-19-api.herokuapp.com/all')
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(data['cases'])
-      let output = '<h3 class= "head" > Total Cases: ' + formatNumber(data['cases']) + '</h3>';
-      output += '<br> <h3 class= "head"> Total Deaths: ' + formatNumber(data['deaths']) + ' </h3>';
-      output += '<br> <h3 class= "head"> Total Recovered: ' + formatNumber(data['recovered']) + '</h3>';
-      output += '<br> <p class= "head" id="time"> Last Updated: ' + data['updated'] + '</p>';
-      document.getElementById('main').innerHTML = output;
-    });
+	// fetch('https://corona.lmao.ninja/all')
+	fetch('https://coronavirus-19-api.herokuapp.com/all').then((response) => response.json()).then((data) => {
+		// console.log(data['cases'])
+		let output = '<h3 class= "head" > Total Cases: ' + formatNumber(data['cases']) + '</h3>';
+		output += '<br> <h3 class= "head"> Total Deaths: ' + formatNumber(data['deaths']) + ' </h3>';
+		output += '<br> <h3 class= "head"> Total Recovered: ' + formatNumber(data['recovered']) + '</h3>';
+		output += '<br> <p class= "head" id="time"> Last Updated: ' + data['updated'] + '</p>';
+		document.getElementById('main').innerHTML = output;
+	});
 
-  // ' + data['updated'] + '
-  // <li>Active: ${formatNumber(country.active)}</li>
+	// ' + data['updated'] + '
+	// <li>Active: ${formatNumber(country.active)}</li>
 
-  // fetch('https://corona.lmao.ninja/countries')
-  fetch('https://coronavirus-19-api.herokuapp.com/countries')
-    .then((response) => response.json())
-    .then((countries) => {
-      let output = '';
-      output += '<ul>';
-      countries.forEach(function (country) {
-        output += `
+	// fetch('https://corona.lmao.ninja/countries')
+	fetch('https://coronavirus-19-api.herokuapp.com/countries')
+		.then((response) => response.json())
+		.then((countries) => {
+			let output = '';
+			output += '<ul>';
+			countries.forEach(function(country) {
+				output += `
               <li class = "data">
                 <b>${country.country}</b>
                   <ul>
@@ -55,11 +54,10 @@ function getData() {
                   </ul>
               </li>
       `;
-      });
-      output += '</ul>';
-      document.getElementById('response').innerHTML = output;
-    });
-
+			});
+			output += '</ul>';
+			document.getElementById('response').innerHTML = output;
+		});
 }
 
 getData();
